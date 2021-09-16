cd5062dc25af5f00112ca2de02a3a9e0c7e8ba49
erikqb3 <erikqb3@gmail.com>
2021-09-15 21:02:07 -0600

    Okay

--- js/AssignmentPortal.js
+++ js/AssignmentPortal.js
@@ -1,33 +1,32 @@
-const getDate = () => {
-    const days = [
-        'Sun.', 'Mon.', 'Tues.', 'Wed.', 'Thurs.', 'Fri.', 'Sat.'
-    ]
-    const months = [
-        'Jan.','Feb.','Mar.','Apr.','May','Jun.','Jul.','Aug.','Sept.','Oct.','Nov.','Dec.'
-    ]
-    const todaysDate = new Date();
-    const actualDate = todaysDate.getDate();
-    const dayName = days[todaysDate.getDay()];
-    const monthName = months[todaysDate.getMonth()];
-    const endings = ['st','nd','rd','th']
-    var datesEnding;
-    if (actualDate == 1) {
-        datesEnding = endings[0];
-    }
-    else if (actualDate == 2) {
-        datesEnding = endings[1];
-    }
-    else if (actualDate == 3) {
-        datesEnding = endings[2];
-    }
-    else if (actualDate >= 4) {
-        datesEnding = endings[3];
-    }
-    
-    const currentDate = dayName + ' ' + actualDate + datesEnding + '-' + monthName + '-' + todaysDate.getFullYear();
-    
-    document.getElementById('currentdate').textContent = currentDate
-    
-    console.log("Hello?")
+const days = [
+    'Sun.', 'Mon.', 'Tues.', 'Wed.', 'Thurs.', 'Fri.', 'Sat.'
+]
+const months = [
+    'Jan.','Feb.','Mar.','Apr.','May','Jun.','Jul.','Aug.','Sept.','Oct.','Nov.','Dec.'
+]
+const todaysDate = new Date();
+const actualDate = todaysDate.getDate();
+const dayName = days[todaysDate.getDay()];
+const monthName = months[todaysDate.getMonth()];
+const endings = ['st','nd','rd','th']
+const hour = todaysDate.getHours();
+var datesEnding;
+if (actualDate == 1) {
+    datesEnding = endings[0];
 }
+else if (actualDate == 2) {
+    datesEnding = endings[1];
+}
+else if (actualDate == 3) {
+    datesEnding = endings[2];
+}
+else if (actualDate >= 4) {
+    datesEnding = endings[3];
+}
+
+const currentDate = dayName + ' ' + actualDate + datesEnding + '-' + monthName + '-' + todaysDate.getFullYear() + " " + hour + ":" + todaysDate.getMinutes();
+
+document.getElementById('currentdate').textContent = currentDate
+
+console.log("Hello?")
 
