b570afe801a3c5d380087cb23a0d2032f89b0c2c
erikqb3 <erikqb3@gmail.com>
2021-10-11 20:41:57 -0600

    Transfer from Week 4 to Week 5

---
+++ Assignments/lesson5/js/preston-4.js
@@ -0,0 +1,19 @@
+let hamburger = document.querySelector("#navMenuButton");
+// console.log(hamburger);
+hamburger.addEventListener('click',toggleNav);
+
+function toggleNav() {
+    document.querySelector("#navBar").classList.toggle("hidden")
+    console.log("Hello")
+}
+
+let startingWindowWidth = window.innerWidth;
+console.log(startingWindowWidth);
+if ((startingWindowWidth > 1450) && (startingWindowWidth < 1650)) {
+    console.log("Hi! I'm Honest!");
+    document.querySelector('#navBar').classList.remove('hidden');
+    document.querySelector('#socialMediaLinks').classList.remove('hidden')
+    document.querySelector("#navMenuButton").classList.add('hidden');
+    document.querySelector('#name').setAttribute('text-size','4em');
+    
+}
