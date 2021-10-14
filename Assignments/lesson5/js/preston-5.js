1c7b5fb55c3a4cf1f4d238ca706f88f3aebe147e
erikqb3 <erikqb3@gmail.com>
2021-10-13 21:30:49 -0600

    All good changes

--- Assignments/lesson5/js/preston-5.js
+++ Assignments/lesson5/js/preston-5.js
@@ -9,11 +9,13 @@ function toggleNav() {
 
 let startingWindowWidth = window.innerWidth;
 console.log(startingWindowWidth);
-if ((startingWindowWidth > 1450) && (startingWindowWidth < 1650)) {
+if (startingWindowWidth > 1000) {
     console.log("Hi! I'm Honest!");
     document.querySelector('#navBar').classList.remove('hidden');
     document.querySelector('#socialMediaLinks').classList.remove('hidden')
     document.querySelector("#navMenuButton").classList.add('hidden');
     document.querySelector('#name').setAttribute('text-size','4em');
-    
+}
+else {
+    document.querySelector('#navBar').classList.add('hidden');
 }
