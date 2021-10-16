2f0d74d0c8b1c6efc6dee2bc334872a5255dc94d
erikqb3 <erikqb3@gmail.com>
2021-10-16 11:42:16 -0600

    fixing site plan and other adjustments

--- Assignments/lesson5/js/preston-5.js
+++ Assignments/lesson5/js/preston-5.js
@@ -10,12 +10,13 @@ function toggleNav() {
 let startingWindowWidth = window.innerWidth;
 console.log(startingWindowWidth);
 if (startingWindowWidth > 1000) {
-    console.log("Hi! I'm Honest!");
+    // console.log("Hi! I'm Honest!");
     document.querySelector('#navBar').classList.remove('hidden');
     document.querySelector('#socialMediaLinks').classList.remove('hidden')
-    document.querySelector("#navMenuButton").classList.add('hidden');
+    // document.querySelector("#navMenuButton").classList.add('hidden');
     document.querySelector('#name').setAttribute('text-size','4em');
 }
 else {
     document.querySelector('#navBar').classList.add('hidden');
+    // ocument.querySelector("#navMenuButton").classList.remove('hidden');
 }
